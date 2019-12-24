61bb853fd704e9c5f9525df0f3e5c8db1901a9a9
Koto Furumiya <kotofurumiya@gmail.com>
2019-12-24 21:49:00 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,40 @@
+# Discord読み上げbot ヘルムホルツ
+
+## Docker
+
+作り途中
+
+## 手動で動かす
+
+依存モジュールをインストールする。ffmpeg必須。
+
+```
+sudo apt install ffmpeg
+npm install
+```
+
+Discordの各種IDを調べて環境変数に書き込む。
+
+各種IDは設定から開発者モードを有効化して「右クリック→IDをコピー」で取得できる。
+
+* DISCORD_TOKEN: botのトークン（開発者ページの「Bot」から取得）
+* DISCORD_GUILD_ID: サーバID
+* DISCORD_CHANNEL_ID: ボイスチャンネルのID
+* DISCORD_SOURCE_CHANNEL_ID: テキストチャンネルのID
+
+Google Cloud Platform で Cloud Text-to-Speech APIを有効にし、サービスアカウントを作る。
+
+* GOOGLE_CLIENT_EMAIL: サービスアカウントのメールアドレス
+* GOOGLE_PRIVATE_KEY: サービスアカウントのプライベートキー
+
+これらの環境変数と一緒に以下を実行する。
+
+```
+export DISCORD_TOKEN="token"
+export DISCORD_GUILD_ID="00000"
+export DISCORD_CHANNEL_ID="11111"
+export DISCORD_SOURCE_CHANNEL_ID="12345"
+export GOOGLE_CLIENT_EMAIL="serviceaccount@mail.com"
+export GOOGLE_PRIVATE_KEY=$'privkey'
+node app.js
+```
---
+++ app.js
@@ -0,0 +1,110 @@
+const Discord = require("discord.js");
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// 環境変数
+const {
+  DISCORD_TOKEN,
+  DISCORD_GUILD_ID,
+  DISCORD_CHANNEL_ID,
+  DISCORD_SOURCE_CHANNEL_ID,
+  GOOGLE_CLIENT_EMAIL,
+  GOOGLE_PRIVATE_KEY
+} = process.env;
+
+let lacksEnv = false;
+
+if(!DISCORD_TOKEN) {
+  lacksEnv = true;
+  console.error('env variable not found: DISCORD_TOKEN');
+}
+
+if(!DISCORD_GUILD_ID) {
+  lacksEnv = true;
+  console.error('env variable not found: DISCORD_GUILD_ID');
+}
+
+if(!DISCORD_CHANNEL_ID) {
+  lacksEnv = true;
+  console.error('env variable not found: DISCORD_CHANNEL_ID');
+}
+
+if(!DISCORD_SOURCE_CHANNEL_ID) {
+  lacksEnv = true;
+  console.error('env variable not found: DISCORD_SOURCE_CHANNEL_ID');
+}
+
+if(!GOOGLE_CLIENT_EMAIL) {
+  lacksEnv = true;
+  console.error('env variable not found: GOOGLE_CLIENT_EMAIL');
+}
+
+if(!GOOGLE_PRIVATE_KEY) {
+  lacksEnv = true;
+  console.error('env variable not found: GOOGLE_PRIVATE_KEY');
+}
+
+if(lacksEnv) {
+  process.exit(1);
+}
+
+// テキスト → base64(mp3)
+// Cloud Text-to-Speech APIを使用
+async function textToSpeechBase64(text) {
+  const request = {
+    input: {text},
+    voice: {languageCode: 'ja'},
+    audioConfig: {audioEncoding: 'MP3'}
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+  const buffer = Buffer.from(response.audioContent);
+  return `data:audio/mpeg;base64,${buffer.toString('base64')}`;
+}
+
+const client = new textToSpeech.TextToSpeechClient({
+  credentials: {
+    client_email: GOOGLE_CLIENT_EMAIL,
+    private_key: GOOGLE_PRIVATE_KEY
+  }
+});
+
+(async function main() {
+  const discordClient = new Discord.Client();
+
+  // ヘルムホルツだけになったらチャンネルから落ちる
+  discordClient.on('voiceStateUpdate', (oldMember, newMember) => {
+    const channel = newMember.voiceChannel || oldMember.voiceChannel;
+    const conn = discordClient.voiceConnections.get(DISCORD_GUILD_ID);
+    if((!channel || channel.members.array().length < 2) && conn) {
+      conn.disconnect();
+    }
+  });
+
+  // ソースとなるテキストチャンネルで発言があった場合、
+  // ボイスチャンネルに参加して発言する
+  discordClient.on('message', async (message) => {
+    const guild = message.guild;
+    const channel = message.member.voiceChannel;
+    if(
+      guild.id !== DISCORD_GUILD_ID ||
+      channel.id !== DISCORD_CHANNEL_ID ||
+      message.channel.id !== DISCORD_SOURCE_CHANNEL_ID
+    ) {
+      return;
+    }
+
+    // 誰もいなかったら参加しない
+    if(channel.members.array().length < 1) { return; }
+    
+    const conn = discordClient.voiceConnections.get(DISCORD_GUILD_ID) || await channel.join();
+    const text = message.content.slice(0, 50);
+
+    conn.playArbitraryInput(await textToSpeechBase64(text));
+  });
+
+  discordClient.once('ready', () => {
+    console.log('Connected to Discord successfully!');
+  });
+  
+  discordClient.login(DISCORD_TOKEN);
+})();
